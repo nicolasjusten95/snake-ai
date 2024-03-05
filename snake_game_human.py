4dd515465e63dc75d8faa7482536b2c97f1f677a
Nicolas Justen <nicolas.justen@sivis.com>
2024-03-05 08:39:11 +0100

    minor adjustments

--- snake_game_human.py
+++ snake_game_human.py
@@ -16,13 +16,12 @@ class Direction(Enum):
 
 Point = namedtuple('Point', 'x, y')
 
-# rgb colors
+# constants
 WHITE = (255, 255, 255)
 RED = (200, 0, 0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0, 0, 0)
-
 BLOCK_SIZE = 20
 SPEED = 10
 
@@ -36,15 +35,12 @@ class SnakeGameHuman:
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
-
-        # init game state
+        # init game
         self.direction = Direction.RIGHT
-
         self.head = Point(self.w / 2, self.h / 2)
         self.snake = [self.head,
                       Point(self.head.x - BLOCK_SIZE, self.head.y),
                       Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
-
         self.score = 0
         self.food = None
         self.place_food()
